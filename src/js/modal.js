049935a2723bc23e4b57b4d81cd2a72e37622f99
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2024-02-21 03:00:20 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Wait for the DOM to be fully loaded before attaching event listeners
 
-  var mobileMenuButton = document.querySelector('.header-btn');
-  var closeMobileMenuButton = document.querySelector('.header-mobile-close');
-  var mobileMenu = document.querySelector('.header-mobile');
+  const mobileMenuButton = document.querySelector('.header-btn');
+  const closeMobileMenuButton = document.querySelector('.header-mobile-close');
+  const mobileMenu = document.querySelector('.header-mobile');
 
   if (mobileMenuButton && closeMobileMenuButton && mobileMenu) {
     // Open mobile menu
